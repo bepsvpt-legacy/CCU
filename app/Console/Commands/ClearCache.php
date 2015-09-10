270dbb27b3b6b42e43bc7720af3c6486d1c3e42f
BePsvPT <freedom@csie.io>
2015-09-10 23:41:25 +0800

    add clear cache command

---
+++ app/Console/Commands/ClearCache.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Cache\CacheManager;
+use Illuminate\Console\Command;
+
+class ClearCache extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cache:clear {key? : The key to be deleted.}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear cache data for production update.';
+
+    /**
+     * @var CacheManager
+     */
+    private $cacheManager;
+
+    /**
+     * @var array
+     */
+    private $keys = [
+        'categories',
+        'newestCoursesComments',
+        'newestCoursesExams',
+        'roommatesStatus',
+    ];
+
+    /**
+     * Create a new command instance.
+     *
+     * @param CacheManager $cacheManager
+     */
+    public function __construct(CacheManager $cacheManager)
+    {
+        parent::__construct();
+
+        $this->cacheManager = $cacheManager;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if (null !== ($key = $this->argument('key')))
+        {
+            $this->keys = [$key];
+        }
+
+        foreach ($this->keys as $key)
+        {
+            $this->comment("Deleting {$key}...");
+            $this->cacheManager->store()->forget($key);
+        }
+
+        $this->info(PHP_EOL . 'Succeed!');
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \App\Console\Commands\Inspire::class,
+        \App\Console\Commands\ClearCache::class,
         \App\Console\Commands\DownloadCoursesArchive::class,
         \App\Console\Commands\ImportCoursesData::class
     ];
