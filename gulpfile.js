f1e7d6131425c949b5e9a4c3a050990d3bcfe8f6
BePsvPT <freedom@csie.io>
2015-09-19 09:08:44 +0800

    update npm packages and update gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,38 @@
-var elixir = require('laravel-elixir');
+var gulp       = require('gulp'),
+    concat     = require('gulp-concat'),
+    sass       = require('gulp-sass'),
+    minifyCSS  = require('gulp-minify-css'),
+    uglify     = require('gulp-uglify'),
+    rename     = require("gulp-rename");
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir.config.sourcemaps = false;
-
-elixir(function(mix) {
-    mix.sass([
-            'ccu.scss'
-        ], 'resources/views/css/ccu.css.php')
-        .scripts([
-            '../../views/js/angular.js.php',
-            '../../views/js/routes.js.php',
-            '../../views/js/controllers/*.js.php',
-            '../../views/js/directives/*.js.php',
-            '../../views/js/factories/*.js.php'
-        ], 'resources/views/js/all.js.php');
+gulp.task('sass', function () {
+    gulp.src('./resources/assets/sass/*.scss')
+        .pipe(sass())
+        .pipe(minifyCSS())
+        .pipe(rename(function(path) {
+            path.basename = "ccu.css";
+            path.extname = ".php";
+        }))
+        .pipe(gulp.dest('./resources/views/css/'));
 });
+
+var scriptsPrefix = './resources/views/js/';
+
+gulp.task('js', function () {
+    gulp.src([
+        scriptsPrefix + 'angular.js.php',
+        scriptsPrefix + 'routes.js.php',
+        scriptsPrefix + 'controllers/*.js.php',
+        scriptsPrefix + 'directives/*.js.php',
+        scriptsPrefix + 'factories/*.js.php'
+    ])
+        .pipe(concat('temp.js'))
+        .pipe(uglify())
+        .pipe(rename(function(path) {
+            path.basename = "all.js";
+            path.extname = ".php";
+        }))
+        .pipe(gulp.dest('./resources/views/js/'));
+});
+
+gulp.task('default',['sass','js']);
