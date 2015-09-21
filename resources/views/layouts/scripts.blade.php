b1be03af687ca54110bb8662426723d6f79cbb63
BePsvPT <freedom@csie.io>
2015-09-21 20:49:53 +0800

    increment vendors version

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,4 +1,4 @@
-<script src="https://cdn.bepsvpt.net/js/vendors.min.js?v=1.1" defer></script>
+<script src="https://cdn.bepsvpt.net/js/vendors.min.js?v=1.2" defer></script>
 <script src="https://cdn.bepsvpt.net/vendor/textboxio/textboxio.js" defer></script>
 <script src="https://www.google.com/recaptcha/api.js" defer></script>
 
