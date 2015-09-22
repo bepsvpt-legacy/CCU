2e206b62cb0dfb75d35bebf6a7753ebc701282a2
BePsvPT <freedom@csie.io>
2015-09-22 16:38:28 +0800

    fix css bug and add facebook page icon

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,7 +5,9 @@
                 <span><a href="https://bepsvpt.net" target="_blank" class="navbar-link">bepsvpt.net</a> © 2015</span>
             </div>
             <div class="col-xs-3 col-sm-3">
-                <a href="https://github.com/BePsvPT/CCU" target="_blank"><span class="fa fa-github fa-lg" aria-hidden="true"></span></a>
+                <a href="https://www.facebook.com/Changing.CCU" target="_blank" data-toggle="tooltip" data-placement="top" title="粉絲專頁"><span class="fa fa-facebook-f fa-lg" aria-hidden="true"></span></a>
+                <span>&nbsp;</span>
+                <a href="https://github.com/BePsvPT/CCU" target="_blank" data-toggle="tooltip" data-placement="top" title="GitHub"><span class="fa fa-github fa-lg" aria-hidden="true"></span></a>
             </div>
         </div>
     </div>
