2088cb7380f5c62e463d123e4ae36c705b82ed2e
Chun-Hung Tseng <henrybear327@users.noreply.github.com>
2016-01-09 23:23:30 +0800

    Update year in footer XD

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row text-center">
             <div class="col-xs-6 col-xs-offset-1 col-sm-3 col-sm-offset-3">
-                <span><a href="https://bepsvpt.net" target="_blank" class="navbar-link">bepsvpt.net</a> © 2015</span>
+                <span><a href="https://bepsvpt.net" target="_blank" class="navbar-link">bepsvpt.net</a> © 2016</span>
             </div>
             <div class="col-xs-3 col-sm-3">
                 <a href="https://www.facebook.com/Changing.CCU" target="_blank" data-toggle="tooltip" data-placement="top" title="粉絲專頁"><span class="fa fa-facebook-f fa-lg" aria-hidden="true"></span></a>
